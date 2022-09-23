9a12361686ffa9a11b075793ffea0b15d16e76bd
NxReason <arsiddde@gmail.com>
2022-09-24 01:07:17 +0300

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('*', (req, res) => {
+  res.send('Hello from NxReason');
+});
+
+app.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
